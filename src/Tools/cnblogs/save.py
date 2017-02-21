4f6caadaa6afe64f276ed1f04e6b19ff57328ca0
fanpan26 <fanpan26@126.com>
2017-02-21 17:51:07 +0800

    add note

--- src/Tools/cnblogs/save.py
+++ src/Tools/cnblogs/save.py
@@ -9,22 +9,27 @@ def writeToTxt(list_name,file_path):
         fp = open(file_path,"w+",encoding='utf-8')
         l = len(list_name)
         i = 0
+        #添加左中括号
         fp.write('[')
         for item in list_name:
+            #直接将项目write到 json文件中
             fp.write(item)
+            #添加每一项之间的逗号
             if i<l-1:
                 fp.write(',\n')
             i += 1
         fp.write(']')
+        #添加右中括号
         fp.close()
     except IOError:
         print("fail to open file")
 
 #def getStr(item):
-#   return json.dumps(item).replace('\'','\"')+',\n'
+#之前用这段代码处理item，后来发现，不用处理，直接保存反而更好，自己处理了，会导致博客中乱七八糟的字符影响反序列化
+#   return str(item).replace('\'','\"')+',\n'
 
 def saveBlogs():
-    for i in range(1,100):
+    for i in range(1,2):
         print('request for '+str(i)+'...')
         blogs = match.blogParser(i,10)
         #保存到文件
