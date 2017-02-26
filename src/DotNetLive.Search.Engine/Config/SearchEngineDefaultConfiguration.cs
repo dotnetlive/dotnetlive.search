207958e542411c6e784d3bf0957cb09bee508adc
fanpan26 <fanpan26@126.com>
2017-02-26 23:05:56 +0800

    update config

--- src/DotNetLive.Search.Engine/Config/SearchEngineDefaultConfiguration.cs
+++ src/DotNetLive.Search.Engine/Config/SearchEngineDefaultConfiguration.cs
@@ -6,26 +6,15 @@ namespace DotNetLive.Search.Engine.Config
 {
     internal class SearchEngineDefaultConfiguration : ISearchEngineConfiguration
     {
+
         /// <summary>
         /// 获取配置
         /// </summary>
         /// <returns></returns>
         public IEnumerable<ConfigNode> GetConfig()
         {
-            //获取的不正确
-            //IConfiguration configuration = new ConfigurationBuilder()
-            //    .AddJsonFile("search.json")
-            //    .Build().GetSection("ElasticSetting");
-
-            //IServiceCollection services = new ServiceCollection();
-
-            //IOptions<ElasticSetting> optionsAccessor = services.AddOptions()
-            //    .Configure<ElasticSetting>(configuration)
-            //    .BuildServiceProvider()
-            //    .GetService<IOptions<ElasticSetting>>();
-            //return optionsAccessor.Value?.Nodes;
             return new List<ConfigNode> {
-                 new ConfigNode { Port=9200, Uri="http://localhost" }
+                new ConfigNode { Port=9200, Uri="http://localhost" }
             };
         }
     }
